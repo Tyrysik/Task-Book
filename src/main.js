eb282a81518eb35cadcd34ac77db625ce9360415
Taras <tyrysik@ukr.net>
2019-12-12 16:04:04 +0200

    Book

---
+++ src/main.js
@@ -0,0 +1,12 @@
+export default function loadApp() {
+    $('#book').turn({
+        width: 922,
+        height: 600,
+        elevation: 50,
+        gradients: true,
+        autoCenter: true
+    });
+}
+
+loadApp();
+
